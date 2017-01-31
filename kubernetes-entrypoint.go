23aff4b2c8ac3164e37993fb960940ffa6d0a1f1
Antoni Rościszewski <antoni.rosciszewski@intel.com>
2017-01-31 09:40:14 +0100

    Change 'interval' to something more meaningful

--- kubernetes-entrypoint.go
+++ kubernetes-entrypoint.go
@@ -27,6 +27,7 @@ func main() {
 		logger.Error.Printf("Creating entrypoint failed: %v", err)
 		os.Exit(1)
 	}
+
 	entrypoint.Resolve()
 
 	if comm = env.SplitEnvToList("COMMAND", " "); len(comm) == 0 {
@@ -34,8 +35,8 @@ func main() {
 		// is an init-container
 		logger.Warning.Printf("COMMAND env is empty")
 		os.Exit(0)
-
 	}
+
 	if err = command.Execute(comm); err != nil {
 		logger.Error.Printf("Cannot execute command: %v", err)
 		os.Exit(1)
