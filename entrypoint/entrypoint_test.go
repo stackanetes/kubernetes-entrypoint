2a57fb841e99956f2660ce0e0bc8679799d220f7
Antoni Rościszewski <antoni.rosciszewski@intel.com>
2017-02-01 23:07:24 +0100

    Add entrypoint tests

--- entrypoint/entrypoint_test.go
+++ entrypoint/entrypoint_test.go
@@ -1,8 +1,26 @@
 package entrypoint
 
 import (
-	"testing"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	cli "github.com/stackanetes/kubernetes-entrypoint/client"
+	"github.com/stackanetes/kubernetes-entrypoint/logger"
+	"github.com/stackanetes/kubernetes-entrypoint/mocks"
 )
+
+const (
+	testNamespace     = "test"
+	dummyResolverName = "dummy"
+	loggerInfoText    = "Entrypoint INFO: "
+)
+
+var testEntrypoint EntrypointInterface
+var testClient cli.ClientInterface
 
 type dummyResolver struct {
 	name      string
@@ -16,10 +34,77 @@ func (d dummyResolver) GetName() (name string) {
 	return d.name
 }
 
-func TestRegisterNewDependency(t *testing.T) {
-	dummy := dummyResolver{name: "dummy"}
-	Register(dummy)
-	if len(dependencies) != 1 {
-		t.Errorf("Expecting dependencies len to be 1 got %v", len(dependencies))
-	}
+func init() {
+	testClient = mocks.NewClient()
+	testEntrypoint = mocks.NewEntrypoint(testClient, testNamespace)
+
 }
+
+func registerNilResolver() {
+	Register(nil)
+}
+
+var _ = Describe("Entrypoint", func() {
+
+	dummy := dummyResolver{name: dummyResolverName}
+
+	BeforeEach(func() {
+		logger.Info.SetFlags(0)
+		logger.Warning.SetFlags(0)
+		logger.Error.SetFlags(0)
+	})
+
+	AfterEach(func() {
+		// Clear dependencies
+		dependencies = make([]Resolver, 0)
+	})
+
+	It("registers new nil resolver", func() {
+		defer GinkgoRecover()
+
+		Ω(registerNilResolver).Should(Panic())
+	})
+
+	It("registers new non-nil resolver", func() {
+		defer GinkgoRecover()
+		Register(dummy)
+		Expect(len(dependencies)).To(Equal(1))
+	})
+
+	It("checks Client() method", func() {
+		client := testEntrypoint.Client()
+		Expect(client).To(Equal(testClient))
+	})
+
+	It("checks Namespace() method", func() {
+		ns := testEntrypoint.GetNamespace()
+		Expect(ns).To(Equal(testNamespace))
+	})
+
+	It("resolves main entrypoint with a dummy dependency", func() {
+		defer GinkgoRecover()
+
+		// Set output logger to our reader
+		r, w, _ := os.Pipe()
+		tmp := os.Stdout
+		defer func() {
+			os.Stdout = tmp
+		}()
+
+		logger.Info.SetOutput(w)
+
+		os.Stdout = w
+		go func() {
+			mainEntrypoint := Entrypoint{client: mocks.NewClient(), namespace: "main"}
+			Register(dummy)
+			mainEntrypoint.Resolve()
+			w.Close()
+		}()
+
+		// Wait for resolver to finish
+		time.Sleep(5 * time.Second)
+
+		stdout, _ := ioutil.ReadAll(r)
+		Expect(string(stdout)).To(Equal(fmt.Sprintf("%sResolving %s\n%sDependency %s is resolved\n", loggerInfoText, dummyResolverName, loggerInfoText, dummyResolverName)))
+	})
+})
--- mocks/entrypoint.go
+++ mocks/entrypoint.go
@@ -1,7 +1,6 @@
 package mocks
 
 import (
-	//	"github.com/stackanetes/kubernetes-entrypoint/entrypoint"
 	cli "github.com/stackanetes/kubernetes-entrypoint/client"
 )
 
@@ -16,12 +15,14 @@ func (m MockEntrypoint) Resolve() {
 func (m MockEntrypoint) Client() (client cli.ClientInterface) {
 	return m.client
 }
+
 func (m MockEntrypoint) GetNamespace() (namespace string) {
 	return m.namespace
 }
-func NewEntrypoint() MockEntrypoint {
+
+func NewEntrypoint(client cli.ClientInterface, namespace string) MockEntrypoint {
 	return MockEntrypoint{
-		client:    NewClient(),
-		namespace: "test",
+		client:    client,
+		namespace: namespace,
 	}
 }
